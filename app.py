10457522229179f7f87e380ec22883ac61fde6d9
sunghoonkim <90130141+sunghoonking@users.noreply.github.com>
2021-10-29 19:22:45 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,70 +1,154 @@
 from datetime import datetime
-
+import jwt
 from flask import Flask, render_template, jsonify, request
 from pymongo import MongoClient
-
+import bcrypt
+import re
 app = Flask(__name__)
 
 client = MongoClient("mongodb://localhost:27017/")
 db = client.dbStock
-
+SECRET = 'f3cfe9ed8fae309f02079dbf'
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
 
-@app.route('/post', methods=['POST'])
+@app.route('/article', methods=['POST'])
 def save_post():
     title = request.form.get('title')
     content = request.form.get('content')
-    post_count = db.post.count()
-    if post_count == 0:
+    article_count = db.article.count()
+    if article_count == 0:
         max_value = 1
     else:
-        max_value = db.post.find_one(sort=[("idx", -1)])['idx'] + 1
+        max_value = db.article.find_one(sort=[("idx", -1)])['idx'] + 1
 
     post = {
         'idx': max_value,
         'title': title,
         'content': content,
-        'reg_date': datetime.now(),
-        'like': 0
+        'read_count': 0,
+        'reg_date': datetime.now()
     }
-    db.post.insert_one(post)
+    db.article.insert_one(post)
     return {"result": "success"}
 
 
-@app.route('/post', methods=['GET'])
-def get_post():
-    posts = list(db.post.find({}, {'_id': False}).sort([("reg_date", -1)]))
-    for a in posts:
+@app.route('/articles', methods=['GET'])
+def get_posts():
+    order = request.args.get('order')
+    per_page = request.args.get('perPage')
+    cur_page = request.args.get('curPage')
+    search_title = request.args.get('searchTitle')
+    search_condition = {}
+    if search_title is not None:
+        search_condition = {"title": {"$regex": search_title}}
+
+    limit = int(per_page)
+    skip = limit * (int(cur_page) - 1)
+    total_count = db.article.find(search_condition).count()
+    total_page = int(total_count / limit) + (1 if total_count % limit > 0 else 0)
+
+    if order == "desc":
+        articles = list(db.article.find(search_condition, {'_id': False})
+                        .sort([("read_count", -1)]).skip(skip).limit(limit))
+    else:
+        articles = list(db.article.find(search_condition, {'_id': False})
+                        .sort([("reg_date", -1)]).skip(skip).limit(limit))
+
+    for a in articles:
         a['reg_date'] = a['reg_date'].strftime('%Y.%m.%d %H:%M:%S')
 
-    return jsonify({"posts": posts})
+    paging_info = {
+        "totalCount": total_count,
+        "totalPage": total_page,
+        "perPage": per_page,
+        "curPage": cur_page,
+        "searchTitle": search_title
+    }
+
+    return jsonify({"articles": articles, "pagingInfo": paging_info})
 
 
-@app.route('/post', methods=['DELETE'])
+@app.route('/article', methods=['DELETE'])
 def delete_post():
     idx = request.args.get('idx')
-    db.post.delete_one({'idx': int(idx)})
+    db.article.delete_one({'idx': int(idx)})
     return {"result": "success"}
 
 
-@app.route('/api/like', methods=['POST'])
-def like_star():
+@app.route('/article', methods=['GET'])
+def get_post():
+    idx = request.args['idx']
+    article = db.article.find_one({'idx': int(idx)}, {'_id': False})
+    return jsonify({"article": article})
+
+
+@app.route('/article', methods=['PUT'])
+def update_post():
+    idx = request.form.get('idx')
     title = request.form.get('title')
-
-    target_post = db.post.find_one({'title': title})
-    current_like = target_post['like']
-
-    new_like = current_like + 1
-
-    db.post.update_one({'title': title}, {'$set': {'like': new_like}})
-
-    return jsonify({'msg': '좋아요 완료!'})
+    content = request.form.get('content')
+    db.article.update_one({'idx': int(idx)}, {'$set': {'title': title, 'content': content}})
+    return {"result": "success"}
 
 
+@app.route('/article/<idx>', methods=['PUT'])
+def update_read_count(idx):
+    db.article.update_one({'idx': int(idx)}, {'$inc': {'read_count': 1}})
+    article = db.article.find_one({'idx': int(idx)}, {'_id': False})
+    return jsonify({"article": article})
+
+
+# 회원가입
+@app.route('/sign-up', methods=['POST'])
+def sign_up():
+    nick_name = request.form['nick_name']
+    password = request.form['password']
+    password_validation = re.compile('^[a-zA-Z0-9]{6,}$')
+
+
+
+    # 비밀번호 유효성검사확인
+    if not password_validation.match(password):
+        return jsonify({"msg": "영어 또는 숫자로 6글자 이상으로 작성해주세요"})
+
+    # 비밀번호 암호화
+    byte_password = password.encode("utf-8")
+    encode_password = bcrypt.hashpw(byte_password, bcrypt.gensalt())
+    decode_password = encode_password.decode("utf-8")
+    doc = {
+        'nick_name': nick_name,
+        'password': decode_password,
+        'team': None,
+
+    }
+    db.user.insert_one(doc)
+
+
+    return jsonify({'msg': '저장완료'})
+
+
+# 로그인
+@app.route('/login', methods=['POST'])
+def login():
+    nick_name = request.form['nick_name']
+    password = request.form['password']
+
+    # 닉네임 확인
+    user = db.user.find_one({'nick_name': nick_name})
+    if user is None:
+        return jsonify({"msg": "INVALID_NICKNAME"})
+
+    # 비밀번호 확인
+    if not bcrypt.checkpw(password.encode("utf-8"), user['password'].encode("utf-8")):
+        return jsonify({"msg": "INVALID_PASSWORD"})
+
+    # JWT 토큰 발행
+    access_token = jwt.encode({"id": str(user['_id'])}, SECRET, algorithm="HS256")
+    return jsonify({"msg": "SUCCESS", "access_token": access_token}), 201
 
 
 if __name__ == "__main__":
